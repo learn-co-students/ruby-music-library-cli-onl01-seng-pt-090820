bd561a5dea8711b7ba03fea019b23332a1fac4a0
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-10-13 20:36:24 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,10 @@
-class Artist 
+class Artist
+  extend Concerns::Findable
+
   attr_accessor :name 
   attr_reader :songs
     @@all = []
-  
+    
   def initialize(name)
     @name = name
     @songs = []
--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,6 @@
 class Song
+  extend Concerns::Findable
+
   attr_accessor :name 
   attr_reader :artist, :genre
   @@all = []
@@ -37,14 +39,6 @@ class Song
     if !genre.songs.include?(self)
       genre.songs << self
    end
-  end
-  
-  def self.find_by_name(name)
-    @@all.find{|song| song.name == name}
-  end
-  
-  def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.create(name)
   end
 
 end
