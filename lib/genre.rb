a45ff96a3aa0cf63410ef46998a07341d86fbf10
Crystal Norman <cnorm88@gmail.com>
2020-10-21 17:52:02 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,40 @@
+class Genre
+  extend Concerns::Findable
+
+  attr_accessor :name
+  attr_reader :songs
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all = @@all.clear
+  end
+  
+  def save 
+    @@all << self
+  end
+  
+  def self.create(name)
+    instance = Genre.new(name)
+    instance.save
+    instance
+  end
+  
+  def artists 
+    artists = [] 
+    songs.map do |song|
+      artists << song.artist
+    end 
+      artists.uniq 
+  end
+   
+end
