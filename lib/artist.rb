5eb1b6e2c3519da2c7734171c1f20fc9a5db8fb1
Chase090 <carloyam21@icloud.com>
2020-10-28 00:59:20 -0700

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,46 @@
+class Artist
+    attr_accessor :name
+    attr_reader :songs
+
+
+    @@all = []
+
+    def initialize(name)
+        @name = name 
+        @songs = []
+    end
+
+
+    def self.all
+        @@all
+    end
+
+    def self.destroy_all
+        @@all = @@all.clear
+    end
+
+
+    def save
+        @@all << self
+    end
+
+    def self.create(name)
+        instance = Artist.new(name)
+        instance.save
+        instance
+    end
+
+    def add_song(song)
+        song.artist = self if song.artist == nil
+        self.songs << song unless @songs.include?(song)
+      end
+      
+    def genres
+        genres = []
+        songs.map do |song|
+            genres << song.genre
+        end
+        genres.uniq
+    end
+        
+end
